07a79954ff2fd41f74591cf79456fa6633d38cd1
Art <d.art.shishkin@gmail.com>
2021-03-21 22:29:54 +0200

    53. Step 33 - Creating Post Entity and Many to One Relationship with User Entity (done in 26. Step 13) (#1)

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -3,3 +3,7 @@ insert into user (id, name, birth_date) values (102, 'Kate', '1983-02-13');
 insert into user (id, name, birth_date) values (103, 'Arina', '2010-07-23');
 insert into user (id, name, birth_date) values (104, 'Nazar', '2016-01-01');
 insert into user (id, name, birth_date) values (105, 'Kuzya', sysdate);
+
+insert into post (id, title, content, user_id) values (201, 'Best course', 'Post 201 content', 101);
+insert into post (id, title, content, user_id) values (202, 'Title 202', 'Post 202 content', 101);
+insert into post (id, title, content, user_id) values (203, 'Title 203', 'Post 203 content', 102);
