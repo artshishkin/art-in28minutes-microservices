802a00214d9b7b111fc5d6796de6cdf2ab2d449a
Art <d.art.shishkin@gmail.com>
2021-03-22 14:28:32 +0200

    140. Step 13 - Configure JPA and Initialized Data - V2 (#2)

--- currency-exchange-service/src/main/java/net/shyshkin/study/microservices/currencyexchangeservice/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/net/shyshkin/study/microservices/currencyexchangeservice/model/CurrencyExchange.java
@@ -5,16 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class CurrencyExchange {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple) values (10001, 'USD', 'INR', 65.0);
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple) values (10002, 'USD', 'UAH', 27.69);
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple) values (10003, 'USD', 'EUR', 0.84);
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple) values (10004, 'USD', 'PLN', 3.86);
