064948186c5504a5840fa3e7a0f1a273823455fd
Art <d.art.shishkin@gmail.com>
2021-03-23 10:25:27 +0200

    150. Step 20 - Connect Currency Conversion & Currency Exchange Microservices - V2 (#2)

--- currency-conversion-service/src/main/java/net/shyshkin/study/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/net/shyshkin/study/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package net.shyshkin.study.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
